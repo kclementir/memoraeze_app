9dc26fa2d528c516083ffe53b27db693402473e4
kclementir <124416160+kclementir@users.noreply.github.com>
2024-05-12 22:41:06 +0800

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'screens/home.dart';
+import 'screens/notes.dart';
+import 'screens/create.dart';
+import 'screens/library.dart';
+import 'screens/profile.dart';
+import 'classes/folder_manager.dart'; // Import the FolderManager
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +13,257 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Memoraeze',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: const Color(0xFF102F50),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const DashboardScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _DashboardScreenState createState() => _DashboardScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _DashboardScreenState extends State<DashboardScreen> {
+  int _selectedIndex = 0;
 
-  void _incrementCounter() {
+  static final List<Widget> _widgetOptions = <Widget>[
+    const HomeScreen(),
+    const NotesScreen(),
+    const CreateSetScreen(),
+    const LibraryScreen(),
+    const ProfileScreen(),
+  ];
+
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: _buildBottomNavigationBar(),
+    );
+  }
+
+  Widget _buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.note), label: 'Notes'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle_outline_rounded), label: 'Create'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.library_books), label: 'Library'),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+      ],
+      currentIndex: _selectedIndex,
+      selectedItemColor: const Color(0xFFC3D1DB),
+      unselectedItemColor: const Color(0xFF306C97),
+      selectedLabelStyle: const TextStyle(color: Color(0xFFC3D1DB)),
+      unselectedLabelStyle: const TextStyle(color: Color(0xFF306C97)),
+      onTap: (index) {
+        if (index == 2) {
+          // If "Create" is tapped
+          _showCreateDrawer(context);
+        } else {
+          _onItemTapped(index);
+        }
+      },
+      backgroundColor: Colors.transparent,
+      type: BottomNavigationBarType.fixed,
+      elevation: 0.0,
+    );
+  }
+
+  void _showCreateDrawer(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: Colors.transparent,
+      builder: (context) {
+        return GestureDetector(
+          onVerticalDragUpdate: (details) {},
+          child: SingleChildScrollView(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Color(0xFF2B4057),
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10), // Enhanced touch area
+                    child: const Icon(Icons.drag_handle_rounded,
+                        size: 36, color: Color(0xFFC3D1DB)),
+                  ),
+                  SizedBox(
+                      height: MediaQuery.of(context).size.height *
+                          0.01), // Responsive vertical spacing
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: MediaQuery.of(context).size.width *
+                          0.02, // Responsive horizontal padding
+                    ),
+                    child: Column(
+                      children: [
+                        _buildOptionContainer(
+                          leadingIcon: Icons.book_outlined,
+                          title: 'Study Set',
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const CreateSetScreen())); // Navigate to CreateSetScreen
+                          },
+                        ),
+                        SizedBox(
+                            height: MediaQuery.of(context).size.height *
+                                0.02), // Responsive spacing between options
+                        _buildOptionContainer(
+                          leadingIcon: Icons.folder_open_rounded,
+                          title: 'Folder',
+                          onTap: () {
+                            Navigator.pop(context); // Close the drawer
+                            _showFolderDialog(context); // Then show the folder dialog
+                          },
+                        ),
+                        SizedBox(
+                            height: MediaQuery.of(context).size.height *
+                                0.01), // Space below the folder similar to the handle bar
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildOptionContainer({
+    required IconData leadingIcon,
+    required String title,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 60,
+        padding: EdgeInsets.symmetric(
+          vertical: 20,
+          horizontal: MediaQuery.of(context).size.width *
+              0.04, // Adjusted for responsive design
+        ),
+        decoration: BoxDecoration(
+          color: const Color(0xFF102F50),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          children: [
+            Icon(leadingIcon, color: const Color(0xFFC3D1DB)),
+            SizedBox(
+                width: MediaQuery.of(context).size.width *
+                    0.03), // Responsive spacing
+            Text(title, style: const TextStyle(color: Color(0xFFC3D1DB))),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void _showFolderDialog(BuildContext context) {
+    String newFolderName = '';
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: const Color(0xFF2B4057),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          title: const Text(
+            'Create folder',
+            style: TextStyle(color: Color(0xFFC3D1DB)),
+          ),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TextField(
+                  onChanged: (value) => newFolderName = value,
+                  decoration: InputDecoration(
+                    hintText: 'Folder name',
+                    hintStyle: TextStyle(color: const Color(0xFFC3D1DB).withOpacity(0.6)),
+                    enabledBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white), // Changed to white when not selected
+                    ),
+                    focusedBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF59A6BF)),
+                    ),
+                  ),
+                  style: const TextStyle(color: Color(0xFFC3D1DB)),
+                  cursorColor: const Color(0xFF59A6BF),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Description (optional)',
+                    hintStyle: TextStyle(color: const Color(0xFFC3D1DB).withOpacity(0.6)),
+                    enabledBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white), // Changed to white when not selecStep 4: LibraryScreen (library.dart)ted
+                    ),
+                    focusedBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF59A6BF)),
+                    ),
+                  ),
+                  style: const TextStyle(color: Color(0xFFC3D1DB)),
+                  cursorColor: const Color(0xFF59A6BF),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('CANCEL', style: TextStyle(color: Color(0xFF59A6BF), fontWeight: FontWeight.bold)),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('OK', style: TextStyle(color: Color(0xFF59A6BF), fontWeight: FontWeight.bold)),
+              onPressed: () {
+                if (newFolderName.isNotEmpty) {
+                  FolderManager().addFolder(newFolderName);
+                  Navigator.of(context).pop();
+                }
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
