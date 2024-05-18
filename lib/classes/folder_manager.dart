3a8861da72660c801e92c1b9e80a1cc9ded78391
kclementir <124416160+kclementir@users.noreply.github.com>
2024-05-19 04:14:51 +0800

    update folder_manager.dart

--- lib/classes/folder_manager.dart
+++ lib/classes/folder_manager.dart
@@ -7,13 +7,46 @@ class FolderManager {
 
   FolderManager._internal();
 
-  final List<Map<String, String?>> _folders = [];
+  final List<Map<String, dynamic>> _folders = [];
 
-  List<String> get folders => _folders.map((folder) => folder['name']!).toList();
+  List<String> get folders => _folders.map((folder) => folder['name'] as String).toList();
 
-  List<Map<String, String?>> get folderDetails => List.unmodifiable(_folders);
+  List<Map<String, dynamic>> get folderDetails => List.unmodifiable(_folders);
 
   void addFolder(String folder, {String? description}) {
-    _folders.add({'name': folder, 'description': description});
+    _folders.add({'name': folder, 'description': description, 'sets': []});
+  }
+
+  void addSetToFolder(String folderName, Map<String, dynamic> set, {void Function(String folderName)? onDuplicate}) {
+    for (var folder in _folders) {
+      if (folder['name'] == folderName) {
+        if (!_isSetInFolder(folder, set['title'])) {
+          folder['sets'].add(set);
+        } else {
+          if (onDuplicate != null) {
+            onDuplicate(folderName);
+          }
+        }
+        break;
+      }
+    }
+  }
+
+  bool _isSetInFolder(Map<String, dynamic> folder, String setTitle) {
+    for (var set in folder['sets']) {
+      if (set['title'] == setTitle) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  List<Map<String, dynamic>> getSetsInFolder(String folderName) {
+    for (var folder in _folders) {
+      if (folder['name'] == folderName) {
+        return List<Map<String, dynamic>>.from(folder['sets']);
+      }
+    }
+    return [];
   }
 }
